61a0dcd6b727b1766f7aa3c55ec30d8ce6eacc32
stella kagrimanyan <stella.kagri@gmail.com>
2022-01-06 18:08:35 -0800

    changed portnum

--- server.js
+++ server.js
@@ -36,5 +36,5 @@ app.get('*', (req, res) => {
     res.sendFile('build/index.html');
   });
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`listening on port ${port}`))
